74a89767959cdf52fcfe48e43c3980cd056b62a0
Adi9770380 <sharmaaditi9796@gmail.com>
2026-07-10 15:43:34 +0530

    Add input validation for Employee Management APIs

--- Employee Management API/employees/serializers.py
+++ Employee Management API/employees/serializers.py
@@ -4,3 +4,15 @@ class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model=Employee
         fields="__all__"
+    def validate_Email(self,value):
+        if Employee.objects.filter(Email=value).exists:
+            raise serializers.ValidationError("Email already exist")
+        return value
+    def validate_Phone_No(self,value):
+        if len(value)!=10:
+            raise serializers.ValidationError("Phone no must be 10 digit")
+        return value
+    def validate_Salary(self,value):
+        if value<=0:
+            raise serializers.ValidationError("Salary must be greater than zero")
+        return value
