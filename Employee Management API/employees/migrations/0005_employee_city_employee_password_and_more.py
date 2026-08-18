16efd9334cee89ffb90e3ad593499d3fe598a84d
Adi9770380 <sharmaaditi9796@gmail.com>
2026-08-19 01:21:38 +0530

    Enhance Employee Management System

--- Employee Management API/employees/migrations/0005_employee_city_employee_password_and_more.py
+++ Employee Management API/employees/migrations/0005_employee_city_employee_password_and_more.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='employee',
             name='Profile_Photo',
-            field=models.ImageField(blank=True, default='profile_photos/default.png', null=True, upload_to='profile_photos/'),
+            field=models.ImageField(blank=True, default='profile_photos/default.jpg', null=True, upload_to='profile_photos/'),
         ),
        
     ]
